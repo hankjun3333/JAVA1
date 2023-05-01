4e00c0177f3d3a0c34d34b03b2add0bc412ef373
hankjun3333 <hankjun3333@naver.com>
2023-05-01 18:15:54 +0900

    first commit

---
+++ chapter02/src/sec01/exam02/VariablintializationExample.java
@@ -0,0 +1,18 @@
+package sec01.exam02;
+
+public class VariablintializationExample {
+
+	public static void main(String[] args) {
+		int v1 = 15;
+		
+		if(v1>10) {
+			int v2;
+			v2 = v1 - 10;
+			
+		}
+		// int v3 = v1 + v2;
+		// 변수는 괄호 안에서만 사용해야 된다
+	}	//System.out.println(v2)
+		//sysout 이라쓰고 Ctrl+Space 를 눌러라
+		//int는 정수 (자료형,변수형)
+}
